a10bed059db5313ca3000eef248aad5ab92a113e
Dima Kirichenko <dskirichenko@gmail.com>
2024-04-03 20:56:44 +0000

    fist commit

--- src/02_04b/MLP.cpp
+++ src/02_04b/MLP.cpp
@@ -7,10 +7,14 @@ double frand(){
 
 // Return a new Perceptron object with the specified number of inputs (+1 for the bias).
 Perceptron::Perceptron(size_t inputs, double bias){
-
+	this->bias = bias;
+	this->weights = std::vector<double>(inputs+1);
+	std::generate(this->weights.begin(), this->weights.end(), frand);
 }
 
 // Run the perceptron. x is a vector with the input values.
 double Perceptron::run(std::vector<double> x){
-	return 0.0;
+	x.push_back(this->bias);
+	double sum = std::inner_product(x.begin(), x.end(), this->weights.begin(), 0.0);
+	return this->sigmoid(sum);
 }
