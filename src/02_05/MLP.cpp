1bd1ad61587c71464d448532ae633692de1f23ea
Dima Kirichenko <dskirichenko@gmail.com>
2024-04-04 11:17:36 +0000

    second commmit

--- src/02_05/MLP.cpp
+++ src/02_05/MLP.cpp
@@ -23,9 +23,17 @@ double Perceptron::run(std::vector<double> x){
 
 void Perceptron::set_weights(std::vector<double> w_init){
 	// w_init is a vector of doubles. Organize it as you'd like.
+	// Set the weights of the perceptron to the values in w_init.
+	// Validate that the size of w_init is the same as the size of the weights vector.
+	// If the sizes don't match, throw an exception.
+	// If the sizes do match, set the weights to the values in w_init.
+	if(w_init.size() != weights.size()){
+		throw std::invalid_argument("The size of the input vector does not match the size of the weights vector.");
+	}
+	weights = w_init;
 }
 
 double Perceptron::sigmoid(double x){
 	// Return the output of the sigmoid function applied to x
-	return 0.0;
+	return 1.0 / (1.0 + exp(-x));
 }
